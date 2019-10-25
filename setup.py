5669e3740f6600125c639fe9bb20e8da8ac7f0f5
Henrique Zanoli <hzanoli@gmail.com>
2019-10-25 13:35:54 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='histogramming',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='hzanoli',
+    author_email='',
+    description='', install_requires=['pandas', 'numpy', 'matplotlib']
+)
